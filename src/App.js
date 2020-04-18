9a5ba71aa07635774945a17950f1fdf7a71d2f5c
Gracie Everitt <gracieeveritt@Gracies-MBP.lan>
2020-04-18 02:51:05 -0600

    Update Routes & Home Component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './components/Login'
 import Signup from './components/SignUp'
 import Dashboard from './components/Dashboard'
 import Footer from './components/Footer'
+import Home from './components/Home'
 
 
 function App() {
@@ -16,6 +17,8 @@ function App() {
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/dashboard" render={routerProps => <Dashboard match={routerProps}/>}/>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
             <Redirect to="/"/>
         </Switch>
         <Footer />
