6dc10acd7154c98abdf9e2b2df1c89da6c008360
Brandon Cantello <bcantello@gmail.com>
2020-04-20 23:21:09 -0700

    Creates basic header with nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 function Header() {
     return(
-        <div>Header</div>
+        <nav>
+            <Link to={'/'} style={{textDecoration: 'none'}}>
+                <div><span id={'logo'}>Fetch</span></div>
+            </Link>
+            <Link to={'/'} style={{textDecoration: 'none'}}>
+                <div>Home</div>
+            </Link>
+            <Link to={'/about'} style={{textDecoration: 'none'}}>
+                <div>About</div>
+            </Link>
+        </nav>
     )
 }
 
