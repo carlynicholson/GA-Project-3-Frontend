b34ee33ca985b2e973210f32e7a1dbce0a8e68d7
Gracie Everitt <gracieeveritt@Gracies-MBP.lan>
2020-04-21 14:52:31 -0600

    master update

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,13 @@ import {Route, Switch, Redirect} from "react-router";
 export default function Main() {
     return (
         <Switch>
+            
+            <Route exact path="/" component={Home} />
+            <Route path="/dashboard" component={Dashboard}/>
+            <Route path="/login" component={Login} />
+            
+            <Redirect to="/"/>
+        
             <Redirect to={'/'} />
         </Switch>
     );
