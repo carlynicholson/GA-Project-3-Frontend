7a368482c9d4f858bca134f32ddb6f72f4eb6272
Gracie Everitt <gracieeveritt@Gracies-MBP.lan>
2020-04-21 16:39:45 -0600

    App - includes login functions & state

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
-import React, {useState,useContext} from 'react';
-
+import React, {useState} from 'react';
 import './App.css';
-
-import Home from './components/Home'
 import Main from './components/Main'
-import axios from 'axios'
-import SignUp from './components/SignUp';
 
 export const AppContext = React.createContext()
 
 function App() { 
   const [loggedIn, setLoggedIn] =useState({
-  loggedInStatus: "NOT_LOGGED_IN",
+  loggedInStatus: "Not_Logged_In",
   user:{}
 })  
 
-const checkLoginStatus = () => {
-  axios.get("http://localhost:5150/logged_in",{
-    withCredentials: true
-  }).then(response =>{
-    console.log('logged in?', response)
-    if(response.data.logged_in && loggedIn.loggedInStatus==="NOT_LOGGED_IN"){
-      setLoggedIn({
-        loggedInStatus: "LOGGED_IN",
-        user: response.data.user
-      })
-    } else if(!response.data.logged_in && loggedIn.loggedInStatus==="LOGGED_IN"){
-      setLoggedIn({
-        loggedInStatus: "NOT_LOGGED_IN",
-        user:{}
-      })
-    }
-  }).catch(error =>{
-    console.log('login error', error)
-  }) //need this from backend
-}
-function handleLogin (data){
+const handleLogin = (data) => {
   console.log('App-handleLogin', data)
   setLoggedIn({
-    loggedInStatus: "LOGGED_IN",
+    loggedInStatus: "Logged_In",
     user: data
   })
-  console.log('useState',loggedIn)
 }
 
-function componentDidMount(){
-  checkLoginStatus()
-}
   return (
     <div className="App">
-      <AppContext.Provider>
+      <AppContext.Provider value={handleLogin}>
         <Main />
       </AppContext.Provider>
     </div>
