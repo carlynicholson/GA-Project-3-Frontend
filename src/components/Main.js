928b5584fb5ae0b66420d4710906b65a40dd2dd8
Brandon Cantello <bcantello@gmail.com>
2020-04-21 21:58:37 -0700

    html layout for booking/services

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Switch, Redirect} from 'react-router';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Login from './Login'
 import SignUp from './SignUp'
 import Dashboard from './Dashboard'
@@ -17,7 +17,7 @@ export default function Main(props) {
                 <Route path="/dashboard" component={Dashboard}/>
                 <Route path="/signup" component={SignUp}/>
                 <Route path="/services" component={Services}/>
-                <Redirect to={'/login'}/>
+                {/*<Redirect to={'/login'}/>*/}
             </Switch>
             <Footer/>
         </>
--- src/components/booking/services.js
+++ src/components/booking/services.js
@@ -1,22 +1,27 @@
 import React, {useState} from "react";
+import walkIcon from '../../assets/walk.png';
+import groomIcon from '../../assets/groom.png';
+import overnightIcon from '../../assets/overnight.png';
+import logo from '../../assets/logo-color.png';
 
 export default function Services() {
 	const [service, setService] = useState("");
 
 	return (
 		<div id={'booking-service-content-container'}>
+			<img src={logo}/>
 			<h2 id={'booking-service-title'}>Which service does your pup need?</h2>
 			<div id={'booking-service-icon-container'}>
 				<div className={'booking-service-walk-icon'}>
-					<img src={'/src/assets/walk.png'} alt={'walk'}/>
+					<img src={walkIcon} alt={'walk'}/>
 					<p>Walk</p>
 				</div>
 				<div className={'booking-service-groom-icon'}>
-					<img src={'/src/assets/groom.png'} alt={'groom'}/>
+					<img src={groomIcon} alt={'groom'}/>
 					<p>Groom</p>
 				</div>
 				<div className={'booking-service-overnight-icon'}>
-					<img src={'/src/assets/overnight.png'} alt={'overnight'}/>
+					<img src={overnightIcon} alt={'overnight'}/>
 					<p>Overnight</p>
 				</div>
 			</div>
