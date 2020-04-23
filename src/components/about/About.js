fb264847617e528ad76719e954999d575d3a89c8
carlynicholson <carlypnicholson@gmail.com>
2020-04-23 07:31:37 -0500

    created about page

---
+++ src/components/about/About.js
@@ -0,0 +1,95 @@
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import './About.css';
+
+
+function About() {
+    
+    return(
+        <body className="about-team-body">
+            <div className="about-intro">
+                <h1 className="abouth1">Our Team</h1>
+                <p className="about-team">Meet the team behind Fetch!</p>
+            </div>
+            <div className="team-members">
+                <div className="member-1">
+                    {/* <img className="headshot" src={Calendar} alt='Scott'/> */}
+                    <div className="member-photo"></div>
+                    <h2 className="member-name">Scott Beverly</h2>
+                    <h3 className="member-title">Title</h3>
+                    <p className="about-member">Taxidermy 90's man braid tumblr succulents literally paleo tote bag swag meh salvia art party you probably haven't heard of them sartorial. Etsy franzen pork belly williamsburg heirloom.</p>
+                    <div className="social-links">
+                        <div className="github">
+                            <a href="/" title="GitHub"><i className="icon-social-github"></i></a>
+                        </div>
+                        <div className="linkedin">
+                            <a href="/" title="LinkedIn"><i className="icon-social-linkedin"></i></a>
+                        </div>
+                        <div className="portfolio">
+                            <a href="/" title="Portfolio"><i class="icon-globe"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="member-2">
+                    {/* <img className="headshot" src={Calendar} alt='Gracie'/> */}
+                    <div className="member-photo"></div>
+                    <h2 className="member-name">Gracie Everitt</h2>
+                    <h3 className="member-title">Title</h3>
+                    <p className="about-member">Taxidermy 90's man braid tumblr succulents literally paleo tote bag swag meh salvia art party you probably haven't heard of them sartorial. Etsy franzen pork belly williamsburg heirloom.</p>
+                    <div className="social-links">
+                        <div className="github">
+                            <a href="/" title="GitHub"><i className="icon-social-github"></i></a>
+                        </div>
+                        <div className="linkedin">
+                            <a href="/" title="LinkedIn"><i className="icon-social-linkedin"></i></a>
+                        </div>
+                        <div className="portfolio">
+                            <a href="/" title="Portfolio"><i class="icon-globe"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="member-3">
+                    {/* <img className="headshot" src={Calendar} alt='Carly'/> */}
+                    <div className="member-photo"></div>
+                    <h2 className="member-name">Carly Nicholson</h2>
+                    <h3 className="member-title">Full-Stack Developer</h3>
+                    <p className="about-member">Taxidermy 90's man braid tumblr succulents literally paleo tote bag swag meh salvia art party you probably haven't heard of them sartorial. Etsy franzen pork belly williamsburg heirloom.</p>
+                    <div className="social-links">
+                        <div className="github">
+                            <a href="https://github.com/carlynicholson" title="GitHub"><i className="icon-social-github"></i></a>
+                        </div>
+                        <div className="linkedin">
+                            <a href="https://www.linkedin.com/in/carlypnicholson/" title="LinkedIn"><i className="icon-social-linkedin"></i></a>
+                        </div>
+                        <div className="portfolio">
+                            <a href="https://carlynicholson.github.io/portfolio/" title="Portfolio"><i class="icon-globe"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="member-4">
+                    {/* <img className="headshot" src={Calendar} alt='Brandon'/> */}
+                    <div className="member-photo"></div>
+                    <h2 className="member-name">Brandon Cantello</h2>
+                    <h3 className="member-title">Title</h3>
+                    <p className="about-member">Taxidermy 90's man braid tumblr succulents literally paleo tote bag swag meh salvia art party you probably haven't heard of them sartorial. Etsy franzen pork belly williamsburg heirloom.</p>
+                    <div className="social-links">
+                        <div className="github">
+                            <a href="/" title="GitHub"><i className="icon-social-github"></i></a>
+                        </div>
+                        <div className="linkedin">
+                            <a href="/" title="LinkedIn"><i className="icon-social-linkedin"></i></a>
+                        </div>
+                        <div className="portfolio">
+                            <a href="/" title="Portfolio"><i class="icon-globe"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+    )
+}
+
+export default About;
